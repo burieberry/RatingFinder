61564a6d4ff610d7b731f5733492dea3646d201d
Burcu Noyan <burcunoyan@gmail.com>
2017-10-27 19:08:55 -0400

    static routes render

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, '..', 'node_modules')));
+app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
 
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);

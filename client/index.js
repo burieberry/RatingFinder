c7f7f67731c4b8a5ee9be94f17ce01c4c81379ae
Burcu Noyan <burcunoyan@gmail.com>
2017-10-27 19:42:58 -0400

    create store

--- client/index.js
+++ client/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './store';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const Main = () => (
+  <Provider store={ store }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
+
+ReactDOM.render(<Main />, document.getElementById('root'));

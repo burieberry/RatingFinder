13309c3b677d1bb460c01ab4e36ab9ddf53da62c
Burcu Noyan <burcunoyan@gmail.com>
2017-10-27 19:33:47 -0400

    react setup

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
